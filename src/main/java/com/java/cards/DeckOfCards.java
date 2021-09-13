d4a3e2b4c8a86a6df9a36ee41f947afbfe6e4e2d
jumdemonali <jumdemonali@gmail.com>
2021-09-13 09:05:57 +0530

    Added: Setup initial game with deck of cards

--- src/main/java/com/java/cards/DeckOfCards.java
+++ src/main/java/com/java/cards/DeckOfCards.java
@@ -3,5 +3,20 @@ package com.java.cards;
 public class DeckOfCards {
     public static void main(String[] args) {
         System.out.println("Welcome to deck of cards program!!");
+        int card = 0;
+        String player[] = {"Player 1", "Player 2", "Player 3", "Player 4"};
+        String[] suit = {"Clubs", "Diamond", "Spades", "Hearts"};
+        String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[][] deck = new String[suit.length][rank.length];
+
+        for (int i = 0; i < suit.length; i++) {
+            for (int j = 0; j < rank.length; j++) {
+                deck[i][j] = suit[i] + " " + rank[j];
+                System.out.println(deck[i][j]);
+                card++;
+            }
+            System.out.println(" ");
+        }
+        System.out.println("Total number of cards are:" + card);
     }
 }
